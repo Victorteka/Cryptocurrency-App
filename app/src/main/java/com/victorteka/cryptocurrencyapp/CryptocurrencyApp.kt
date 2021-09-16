092412b805a91a13f7b23a8158a187c6ac12fd89
Victorteka <ambukateka@gmail.com>
2021-09-16 10:34:06 +0300

    add crashlytics and build signed app bundle

--- app/src/main/java/com/victorteka/cryptocurrencyapp/CryptocurrencyApp.kt
+++ app/src/main/java/com/victorteka/cryptocurrencyapp/CryptocurrencyApp.kt
@@ -1,8 +1,31 @@
 package com.victorteka.cryptocurrencyapp
 
 import android.app.Application
+import androidx.annotation.Nullable
+import com.victorteka.core.crashlytics.CrashlyticsTree
 import dagger.hilt.android.HiltAndroidApp
+import org.jetbrains.annotations.NotNull
+import timber.log.Timber
 
 @HiltAndroidApp
-class CryptocurrencyApp: Application() {
+class CryptocurrencyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() = when {
+        BuildConfig.DEBUG -> {
+            Timber.plant(object : Timber.DebugTree() {
+                @Nullable
+                override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        }
+        else -> {
+            Timber.plant(CrashlyticsTree())
+        }
+    }
 }
